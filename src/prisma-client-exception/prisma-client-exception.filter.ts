ddc043fb7edcf51cd7c28e2c1e4d272e82f5a6cf
TangoCharlie <elouanreymond@tutanota.com>
2024-10-05 01:07:08 +0200

    feat: error handling

---
+++ src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -0,0 +1,25 @@
+import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { Response } from 'express';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    switch (exception.code) {
+      case 'P2002': {
+        const status = HttpStatus.CONFLICT;
+        response.status(status).json({
+          statusCode: status,
+          message: 'Unique constraint failed',
+        });
+        break;
+      }
+      default:
+        super.catch(exception, host);
+        break;
+    }
+  }
+}
